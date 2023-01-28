df690a867821e4177996174efbff5f70ded76029
erenaygunn <eaygun178@gmail.com>
2023-01-28 21:02:50 +0300

    added new quotes

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ const quotes = [
     {
       quote: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
       author: "Steve Jobs"
+    },
+    {
+      quote: "In three words I can sum up everything I've learned about life: it goes on.",
+      author: "Robert Frost"
+    },
+    {
+      quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+      author: "Helen Keller"
+    },
+    {
+      quote: "Happiness is not something ready made. It comes from your own actions.",
+      author: "Dalai Lama"
+    },
+    {
+      quote: "Be the change that you wish to see in the world.",
+      author: "Mahatma Gandhi"
+    },
+    {
+      quote: "I can accept failure, everyone fails at something. But I can't accept not trying.",
+      author: "Michael Jordan"
     }
   ]
 
